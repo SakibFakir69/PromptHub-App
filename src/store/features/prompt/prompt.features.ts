f862b2b659338ccd9cfee37e661a0968e37bc86a
Sakib Fakir <FakirSakib22232@gmail.com>
2026-04-12 18:54:48 +0800

    feat(prompt) add get prompt rtk query

--- src/store/features/prompt/prompt.features.ts
+++ src/store/features/prompt/prompt.features.ts
@@ -1,54 +1,53 @@
 import { baseApi } from "../../baseApi";
 
-
-
-
-
 export const promptApi = baseApi.injectEndpoints({
-
-    endpoints:(builder)=>({
-
-        createPrompt:builder.mutation({
-            query:(data)=>({
-                url:'/prompt/create-prompt',
-                method:"POST",
-                body:data,
-
-            }),
-            invalidatesTags:['Prompt']
+  endpoints: (builder) => ({
+    createPrompt: builder.mutation({
+      query: (data) => ({
+        url: "/prompt/create-prompt",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["Prompt"],
+    }),
+    promptImageUpload: builder.mutation({
+      query: (data) => ({
+        url: "/prompt/prompt-image",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    upVote: builder.mutation({
+      query: (id) => ({
+        url: "/prompt/upVote",
+        method: "POST",
+        body: { postId: id },
+      }),
+      invalidatesTags: (id) => [
+        { type: "Prompt", id },
+        { type: "Prompt", id: "LIST" },
+      ],
+    }),
+    downVote: builder.mutation({
+      query: (id) => ({
+        url: "/prompt/downVote",
+        method: "POST",
+        body: { postId: id },
+      }),
+      invalidatesTags: ["Prompt"],
+    }),
+    getPrompt:builder.mutation({
+        query:()=>({
+            url:'/prompt/get-prompt',
+            method:"GET"
         })
-        ,
-        promptImageUpload:builder.mutation({
-            query:(data)=>({
-                url:"/prompt/prompt-image",
-                method:"POST",
-                body:data
-            })
-
-        }),
-        upVote:builder.mutation({
-            query:(id)=>({
-                url:'/prompt/upVote',
-                method:"POST",
-                body:{ postId: id },
-            }),
-              invalidatesTags: (id) => [
-    { type: "Prompt", id },
-    { type: "Prompt", id: "LIST" },
-  ],
-        }),
-          downVote:builder.mutation({
-            query:(id)=>({
-                url:'/prompt/downVote',
-                method:"POST",
-                body:{ postId: id },
-            }),
-             invalidatesTags:['Prompt']
-        })
-
-
     })
+  }),
+});
 
-})
-
-export const {useCreatePromptMutation,usePromptImageUploadMutation , useUpVoteMutation, useDownVoteMutation} = promptApi;
+export const {
+  useCreatePromptMutation,
+  usePromptImageUploadMutation,
+  useUpVoteMutation,
+  useDownVoteMutation,
+} = promptApi;
